a49f4c10b2ce7207596986aeabbad88f47247907
my name <blueshaperd3@gmail.com>
2024-04-16 01:12:20 +0500

    increment

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Ch1 Alert
+//Ch1,2,3,4 Alert
 // alert("Hello World");
 // var a = 5;
 // var b = 10;
@@ -12,7 +12,57 @@
 // console.log(c)
 
 
-var x = 5;
-var y = "Rehan";
-var z = y + x;
-document.write(z);
+// var x = 5;
+// var y = "Rehan";
+// var z = y + x;
+// document.write(z);
+
+
+// var x = "Rafique";
+// var y = "Rehan";
+// var z = y + x;
+// document.write(z);
+
+
+// var f = 20;
+// var g = "24";
+// var h = f + g;
+// alert(h);
+
+
+// var l = "20";
+// var m = "24";
+// var n = l + m;
+// alert(n);
+
+
+// var l = 20;
+// var m = 2;
+// var n = l * m;
+// alert(n);
+
+
+// var l = 20;
+// var m = 8;
+// var n = l + m;
+// alert(n);
+
+
+// var a = 5;
+// var b = a++;
+// alert(b);
+
+
+// var a = 5;
+// var b = ++a;
+// alert(b);
+
+
+// var a = 5;
+// var b = --a;
+// alert(b);
+
+
+var x = 10;
+var y = x++ + ++x - x + x++ - ++x + x;
+alert(y) 
