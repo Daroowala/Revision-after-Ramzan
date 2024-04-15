d0123914c1ec1f310bc309271576790efcd3b0fe
my name <blueshaperd3@gmail.com>
2024-04-15 23:56:00 +0500

    alert 1

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 //Ch1 Alert
-alert("Hello World");
+// alert("Hello World");
+// var a = 5;
+// var b = 10;
+// var c = a + b;
+// alert(c)
+
+
+// var a = 5;
+// var b = 10;
+// var c = a + b;
+// console.log(c)
+
+
+var x = 5;
+var y = "Rehan";
+var z = y + x;
+document.write(z);
