a845efef4a8ccd225a79fdd6e63de07c70c1d116
my name <blueshaperd3@gmail.com>
2024-04-20 02:15:42 +0500

    dimound print

--- app.js
+++ app.js
@@ -261,3 +261,10 @@
 //         console.log(firstName[i] + " " + lastName[x])
 //     }
 // }
+
+for (var i = 0; i <= 10; i++){
+    for (var j = 0; j < i; j++){
+        document.write("*")
+    }
+    document.write("<br>")
+}
